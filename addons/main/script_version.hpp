be9629508da2336f6e22c4924359c2d948a8fb2f
Seb <65898127+Seb105@users.noreply.github.com>
2024-05-12 18:38:16 +0100

    Copy from project

---
+++ addons/main/script_version.hpp
@@ -0,0 +1,4 @@
+#define MAJOR 3
+#define MINOR 3
+#define PATCH 4
+#define BUILD 0
---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Ample Camo Pack";
+author = "Seb";
+actionName = "Website";
+action = "http://www.carpenoctem.co";
+description = "Ample Camo Pack";
+tooltip = "Ample Camo Pack";
+tooltipOwned = "Ample Camo Pack";
+overview = "Ample Camo Pack";
+overviewText = "Ample Camo Pack Overview";
+logoSmall	 = "logoSmall.paa";	    // Display next to the item added by the mod
+logo		 = "logo.paa";			// Logo displayed in the main menu
+logoOver	 = "logoSmall.paa";	
